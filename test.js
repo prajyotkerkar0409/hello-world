04be9c3ed179124e9bc10b372422ffa58c85f04d
prajyot.kerkar <prajyot.kerkar@accenture.com>
2026-06-26 15:24:03 +0530

    deleted files

--- test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const { add } = require('./index');
-
-assert.strictEqual(add(2, 3), 5, 'add(2, 3) should return 5');
-assert.strictEqual(add(-1, 1), 0, 'add(-1, 1) should return 0');
-assert.strictEqual(add(0, 0), 0, 'add(0, 0) should return 0');
-
-console.log('All tests passed.');
