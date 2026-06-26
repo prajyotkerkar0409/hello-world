288d82f19a76f3fbbe69022a76a655caf86f3c97
prajyot.kerkar <prajyot.kerkar@accenture.com>
2026-06-26 17:38:41 +0530

    added Github and Terraform details

---
+++ Terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-terraform-s3-bucket-12345"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
